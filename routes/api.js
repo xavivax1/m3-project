0888be4509f16e96e1c79a8d205c47b00a0b0411
andreucc <andreucc@protonmail.com>
2019-04-02 10:42:53 +0200

    function added to get only higher bid

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,43 @@ const Service = require('../models/Service');
 const Bid = require('../models/Bid');
 const { isLoggedIn } = require('../helpers/middlewares');
 
+//Funcion para retornar elementos unicos de un array de objetos
+ unicos = (arr, filtrar) =>{
+  var unics = [];
+  return arr.filter(function(item){
+    return unics.indexOf(item[filtrar]) < 0 ? unics.push(item[filtrar]) : false
+  })
+}
+
+//Recibir información del perfil
+router.get('/user/me', isLoggedIn(), async (req, res, next) => {
+  const id = req.session.currentUser._id;
+  try {
+    const profile = await User.findById(id)
+    return res.status(200).json(profile);
+  }
+  catch (err) {
+    next(err);
+  }
+});
+
+//Editar perfil
+router.put('/user/:id/edit', isLoggedIn(), async (req, res, next) => {
+  const { id } = req.params;
+  const { username, image, mobile, location } = req.body;
+  const user = {
+    username, image, mobile, location
+  };
+  try {
+    const editedUser = await User.findByIdAndUpdate(id, user)
+    res.status(200).json({ message: "Profile updated", data: editedUser });
+  }
+  catch (err) {
+    next(err);
+  }
+});
+
+//Crear Auction --> Creamos tambien una puja del mismo vendedor con el precio inicial, para así poder buscar por bind
 router.post('/auction/create', isLoggedIn(), async (req, res, next) => {
   const owner = req.session.currentUser._id;
   const { name, description, image, StartingPrice, EndingTime, status } = req.body;
@@ -22,32 +59,7 @@ router.post('/auction/create', isLoggedIn(), async (req, res, next) => {
   }
 });
 
-router.get('/user/me', isLoggedIn(), async (req, res, next) => {
-  const id = req.session.currentUser._id;
-  try {
-    const profile = await User.findById(id)
-    return res.status(200).json(profile);
-  }
-  catch (err) {
-    next(err);
-  }
-});
-
-router.put('/user/:id/edit', isLoggedIn(), async (req, res, next) => {
-  const { id } = req.params;
-  const { username, image, mobile, location } = req.body;
-  const user = {
-    username, image, mobile, location
-  };
-  try {
-    const editedUser = await User.findByIdAndUpdate(id, user)
-    res.status(200).json({ message: "Profile updated", data: editedUser });
-  }
-  catch (err) {
-    next(err);
-  }
-});
-
+//Mis servicios activos
 router.get('/auctions/me', isLoggedIn(), async (req, res, next) => {
   const id = req.session.currentUser._id;
   try {
@@ -55,12 +67,14 @@ router.get('/auctions/me', isLoggedIn(), async (req, res, next) => {
     const list = all.filter((e) => {
       return e.service.owner.equals(id) && e.service.status === true
     })
-    res.status(200).json({ message: 'Auctions list returned', data: list });
+    const uniqueList = unicos(list, "service")
+    res.status(200).json({ message: 'Auctions list returned', data: uniqueList });
   } catch (err) {
     next(err)
   }
 });
 
+//Mis servicios finalizados
 router.get('/auctions/me/finished', isLoggedIn(), async (req, res, next) => {
   const id = req.session.currentUser._id;
   try {
@@ -74,16 +88,73 @@ router.get('/auctions/me/finished', isLoggedIn(), async (req, res, next) => {
   }
 });
 
+//Lista general de auctions
 router.get('/auctions', isLoggedIn(), async (req, res, next) => {
   const id = req.session.currentUser._id;
   try {
-    const list = await Bid.find({ buyer: { $ne: id } }).populate('service').populate('buyer');
+    const prelist = await Bid.find().sort({ price: -1 }).populate('service').populate('buyer');
+    const list = unicos(prelist, "service")
     res.status(200).json({ message: 'Auctions list returned', data: list });
   } catch (err) {
     next(err)
   }
 });
 
+//Lista general de auctions donde he pujado
+router.get('/auctions/bidded', isLoggedIn(), async (req, res, next) => {
+  const id = req.session.currentUser._id;
+  try {
+    // const all = await Bid.find({ buyer: { $eq: id } }).sort({ price: -1 }).populate('service').populate('buyer');
+    const all = await Bid.find().sort({ price: -1 }).populate('service').populate('buyer');
+    const prelist = all.filter((e) => {
+      return e.buyer.equals(id) && e.service.status === true
+    })
+    const list = unicos(prelist, "service")
+    res.status(200).json({ message: 'Auctions list returned', data: list });
+  } catch (err) {
+    next(err)
+  }
+});
+
+//Detalle de la auction
+router.get('/auction/:id', isLoggedIn(), async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const auction = await Bid.find({ service: { $eq: id } }).sort({ price: -1 }).limit(1).populate('service').populate('buyer');
+    res.status(200).json({ message: 'Auction detail', data: auction })
+    console.log(auction);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Borrar servicio y todas sus pujas
+router.delete('/auction/:id', isLoggedIn(), async (req, res, next) => {
+  const { id } = req.params
+  try {
+    await Service.findByIdAndDelete(id);
+    await Bid.deleteMany({ service: { $eq: id } });
+    res.status(200).json({ message: 'Auction deleted' })
+  } catch (err) {
+    next(err)
+  }
+});
+
+//Crear bid
+router.post('/bid/create', isLoggedIn(), async (req, res, next) => {
+  console.log(req.body)
+  const service = req.body.id;
+  const buyer = req.session.currentUser._id;
+  const price = req.body.price
+  const newBid = { service, buyer, price}
+  try {
+    await Bid.create(newBid);
+  } catch (err) {
+    next(err)
+  }
+})
+
+/*
 router.get('/check', isLoggedIn(), async (req, res, next) => {
   const now = moment();
 
@@ -103,26 +174,6 @@ router.get('/check', isLoggedIn(), async (req, res, next) => {
     next(err);
   }
 });
-
-router.get('/auction/:id', isLoggedIn(), async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const auction = await Bid.find({ service: { $eq: id } }).sort({ price: -1 }).limit(1).populate('service').populate('buyer');
-    res.status(200).json({ message: 'Auction detail', data: auction })
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.delete('/auction/:id', isLoggedIn(), async (req, res, next) => {
-  const { id } = req.params
-  try {
-    await Service.findByIdAndDelete(id);
-    await Bid.deleteMany({ service: { $eq: id } });
-    res.status(200).json({ message: 'Auction deleted' })
-  } catch (err) {
-    next(err)
-  }
-});
+*/
 
 module.exports = router;
